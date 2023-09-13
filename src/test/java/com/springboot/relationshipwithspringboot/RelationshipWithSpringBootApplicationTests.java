e6dcd25a8c874c7f1e8325efff9a224595dc53a8
Badrul <badrul.alam@bjitgroup.com>
2023-09-13 15:56:39 +0600

    initial commit

---
+++ src/test/java/com/springboot/relationshipwithspringboot/RelationshipWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.relationshipwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationshipWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
